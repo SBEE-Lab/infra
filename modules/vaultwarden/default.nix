0861b64821d5383277a662c6dbff029b5b4fd18f
mulatta <67085791+mulatta@users.noreply.github.com>
2026-01-30 02:59:51 +0900

    feat(cloudflare): add vault.sjanglab.org DNS record

--- modules/vaultwarden/default.nix
+++ modules/vaultwarden/default.nix
@@ -19,6 +19,7 @@
       SSO_CLIENT_ID = "OfBSHOHF0txEZzpJgZAIahUAjfHSQQ18xNWGwyNV";
       SSO_SCOPES = "email profile openid";
       SSO_PKCE = true;
+      SSO_ALLOW_UNVERIFIED_EMAILS = true;
 
       ROCKET_PORT = 8000;
     };
