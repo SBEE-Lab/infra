724ed1bfce217575efe3636958b78bffd8f35a8d
mulatta <67085791+mulatta@users.noreply.github.com>
2026-04-30 17:02:56 +0900

    users: add yoojin, hyeonah, saebom, dasol

--- modules/users/researchers.nix
+++ modules/users/researchers.nix
@@ -12,26 +12,79 @@ let
     "researcher"
     "input"
   ];
+  dasolKey = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHwTbesocbUbs0EL6OqVcsB4EqB7/fFGmwjaC5dX9cOD"
+  ];
+  hyeonahKey = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDhl8J3I9zHxpaIjcVbo/MooyfBki+d9YGhAv9fmR4bY"
+  ];
+  saebomKey = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFTnfSD0B4AixKN8CRjgISQkEkWZuXAtATTuWy4QoMGm"
+  ];
+  yoojinKey = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICxu/66OKP+sLMOxxQpxvSN0L0WlXKFTz30WwDlt758z"
+  ];
   # ADD YOUR SSH PUBLIC KEY FOR SERVER CONNECTION
   # testUserKeys = [
   # "ssh-ed25519 AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+bbbbbbbbbbbbbbbbbbbbbbbbbbbb"
   # ];
 in
 {
-  # ADD YOUR USER ACCOUNT
-  # users.users = {
-  #   # specify your real name in comments
-  #   testUsers = {
-  #     isNormalUser = true;
-  #     home = "/home/testUser"; # specify home directory paths
-  #     inherit extraGroups;
-  #     shell = "/run/current-system/sw/bin/bash"; # specify your favorite shell
-  #     uid = 3000; # uid should be unique
-  #     allowedHosts = []; # specify allowed host (ex: "rho")
-  #     openssh.authorizedKeys.keys = testUserKeys;
-  #     expires = "2026-08-31"; # for researcher group, expiration must be specified
-  #   };
-  # };
+  users.users = {
+    dasol = {
+      isNormalUser = true;
+      home = "/home/dasol";
+      inherit extraGroups;
+      shell = "/run/current-system/sw/bin/bash";
+      uid = 3000;
+      allowedHosts = [ "psi" ];
+      openssh.authorizedKeys.keys = dasolKey;
+      expires = "2026-08-31";
+    };
+    hyeonah = {
+      isNormalUser = true;
+      home = "/home/hyeonah";
+      inherit extraGroups;
+      shell = "/run/current-system/sw/bin/bash";
+      uid = 3001;
+      allowedHosts = [ "psi" ];
+      openssh.authorizedKeys.keys = hyeonahKey;
+      expires = "2026-08-31";
+    };
+    saebom = {
+      isNormalUser = true;
+      home = "/home/saebom";
+      inherit extraGroups;
+      shell = "/run/current-system/sw/bin/bash";
+      uid = 3002;
+      allowedHosts = [ "psi" ];
+      openssh.authorizedKeys.keys = saebomKey;
+      expires = "2026-08-31";
+    };
+    yoojin = {
+      isNormalUser = true;
+      home = "/home/yoojin";
+      inherit extraGroups;
+      shell = "/run/current-system/sw/bin/bash";
+      uid = 3003;
+      allowedHosts = [ "psi" ];
+      openssh.authorizedKeys.keys = yoojinKey;
+      expires = "2026-08-31";
+    };
+
+    # ADD YOUR USER ACCOUNT
+    # specify your real name in comments
+    # testUsers = {
+    #   isNormalUser = true;
+    #   home = "/home/testUser"; # specify home directory paths
+    #   inherit extraGroups;
+    #   shell = "/run/current-system/sw/bin/bash"; # specify your favorite shell
+    #   uid = 3000; # uid should be unique
+    #   allowedHosts = []; # specify allowed host (ex: "rho")
+    #   openssh.authorizedKeys.keys = testUserKeys;
+    #   expires = "2026-08-31"; # for student group, expiration must be specified
+    # };
+  };
 
   # DANGER ZONE!
   # Make sure all data is backed up before adding user names here. This will
