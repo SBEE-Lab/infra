16eb66e1612f8e6ddca819c94f7d15829a4f0968
mulatta <67085791+mulatta@users.noreply.github.com>
2026-07-02 21:50:50 +0900

    zfs: avoid implicit root pool imports
    Match the upcoming safer NixOS default so hosts do not automatically import stale root pools from another installation during boot.

--- modules/bootloader.nix
+++ modules/bootloader.nix
@@ -1,4 +1,7 @@
 {
   boot.loader.efi.canTouchEfiVariables = false;
   boot.loader.systemd-boot.enable = true;
+
+  # Avoid importing a stale root pool from another installation by default.
+  boot.zfs.forceImportRoot = false;
 }
