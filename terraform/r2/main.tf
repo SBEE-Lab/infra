4a22ad6bab2581dbc60330304bf8172458925f6f
mulatta <67085791+mulatta@users.noreply.github.com>
2026-07-31 03:47:34 +0900

    terraform: provision Stalwart blob storage
    Keep mail bodies outside eta so host-local failure does not remove message content.

--- terraform/r2/main.tf
+++ terraform/r2/main.tf
@@ -9,6 +9,17 @@ resource "cloudflare_r2_bucket" "niks3" {
   }
 }
 
+resource "cloudflare_r2_bucket" "stalwart_mail_blobs" {
+  account_id    = data.sops_file.secrets.data["CLOUDFLARE_ACCOUNT_ID"]
+  name          = "stalwart-mail-blobs"
+  location      = "apac"
+  storage_class = "Standard"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 resource "cloudflare_r2_custom_domain" "niks3_cache" {
   account_id  = data.sops_file.secrets.data["CLOUDFLARE_ACCOUNT_ID"]
   bucket_name = cloudflare_r2_bucket.niks3.name
